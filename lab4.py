95c2915198c743128b638eb657dd3a253c396784
unknown <timthebananalord@gmail.com>
2019-04-26 11:48:58 -0400

    finished lab4

--- lab4.py
+++ lab4.py
@@ -2,7 +2,7 @@
 #Drexel University 2018
 #CS 172
 
-from monster import *
+
 from lab4_monsters import *
 import random
 
@@ -11,17 +11,26 @@ def monster_battle(m1, m2):
 
 	#first reset everyone's health!
 	#####TODO######
+	m1.resetHealth()
+	m2.resetHealth()
 
 	#next print out who is battling
 	print("Starting Battle Between")
 	print(m1.getName()+": "+m1.getDescription())
 	print(m2.getName()+": "+m2.getDescription())
-	
+
 	
 	#Whose turn is it?
 	attacker = None
 	defender = None
-	
+	draw = random.random()
+	if (draw >= 0.5):
+		attacker = m1
+		defender = m2
+	else:
+		attacker = m2
+		defender = m1
+
 	#Select Randomly whether m1 or m2 is the initial attacker
 	#to other is the initial definder
 	######TODO######
@@ -44,24 +53,32 @@ def monster_battle(m1, m2):
 		#for each of these options, apply the appropriate attack and 
 		#print out who did what attack on whom
 		if(move >=1 and move <= 60):
-			#Attacker uses basic attack on defender
-			######TODO######
+			attacker.basicAttack(defender)
+			print("{} used {} on {}".format(attacker.getName(), attacker.basicName(), defender.getName()))
 		elif(move>=61 and move <= 80):
 			#Defend!
-			######TODO######
+			attacker.defenseAttack(defender)
+			print("{} usef {} on {}".format(attacker.getName(), attacker.defenseName(), defender.getName()))
 		else:
 			#Special Attack!
-			######TODO######
+			attacker.specialAttack(defender)
+			print("{} used {} on {}".format(attacker.getName(), attacker.specialName(), defender.getName()))
+		
 		
 		#Swap attacker and defender
 		######TODO######
-		
+		temp = attacker
+		attacker = defender
+		defender = temp
 		#Print the names and healths after this round
 		######TODO######
-		
+		print("{} health: {}\n{} health: {}".format(attacker.getName(), attacker.getHealth(), defender.getName(), defender.getHealth()))
 	#Return who won
 	######TODO######
-
+	if m1.getHealth() <= 0:
+		return "{} wins".format(m2.getName())
+	else:
+		return "{} wins".format(m1.getName())
 #----------------------------------------------------
 if __name__=="__main__":
 	#Every battle should be different, so we need to
@@ -69,11 +86,11 @@ if __name__=="__main__":
 	#With no input Python will set the seed
 	
 	random.seed(0)
-	first = 
-	second = 
+	first = bear()
+	second = lapras()
 	
 	winner = monster_battle(first,second)
 	
 	#Print out who won
 	####TODO####
-	
+	print(winner)
--- lab4_monsters.py
+++ lab4_monsters.py
@@ -1,13 +1,13 @@
-from monster import *
+from monster import monster
 
 class bear(monster):   
-    def __init__(self, name, description, basicName, defenseName, specialName, health):
-        self.__name = "8 F***ing bears"
-        self.__description = "They're 8 bears"
-        self.__basicName = "Claw"
-        self.__defenseName = "Block"
-        self.__specialName = "Hybernate"
-        self.__health = 40
+    def __init__(self, name="8 F***ing bears", description="They're 8 bears", basicName="Claw", defenseName="Block", specialName="Hybernate", health = 40):
+        self.__name = name
+        self.__description = description
+        self.__basicName = basicName
+        self.__defenseName = defenseName
+        self.__specialName = specialName
+        self.__health = health
     def __str__(self):
         return "8 big ol' bears"
 
@@ -43,3 +43,49 @@ class bear(monster):
     
     def resetHealth(self):
         self.__health = 40
+
+class lapras(monster):
+    def __init__(self, name="Lapras", description="A water type pokemon", basicName="Blizzard Burn", defenseName="Block", specialName="Ice Beam", health = 190):
+        self.__name = name
+        self.__description = description
+        self.__basicName = basicName
+        self.__defenseName = defenseName
+        self.__specialName = specialName
+        self.__health = health
+    
+    def __str__(self):
+        return str(self.__description)
+    
+    def getName(self):
+        return self.__name
+    
+    def getDescription(self):
+        return self.__description
+
+    def basicAttack(self, enemy):
+        enemy.doDamage(160)
+
+    def basicName(self):
+        return self.__basicName
+
+    def defenseAttack(self, enemy):
+        self.doDamage(-10)
+
+    def defenseName(self):
+        return self.__defenseName
+
+    def specialAttack(self, enemy):
+        enemy.doDamage(100)
+        self.resetHealth()
+
+    def specialName(self):
+        return self.__specialName
+
+    def getHealth(self):
+        return self.__health
+
+    def doDamage(self, damage):
+        self.__health -= damage
+
+    def resetHealth(self):
+        self.__health = 160
