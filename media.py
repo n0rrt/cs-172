eb02ad65dff27493f7ed55c1c3fcdd7512690063
unknown <timthebananalord@gmail.com>
2019-04-29 10:08:49 -0400

    changes to media.py

--- media.py
+++ media.py
@@ -22,6 +22,13 @@ class Movie(Media):
    
     def play(self):
         print("{}, playing now".format(self.__name))
+
+    def getDirector(self):
+        return self.__director
+    
+    def getRuntime(self):
+        return self.__runtime
+
 class Song(Media):
     def __init__(self, media_type, name, rating, artist, album):
         self.__artist = artist
@@ -32,4 +39,22 @@ class Song(Media):
     
     def play(self):
         print("{} by {}, now playing".format(self.__name, self.__artist))
+
+    def getArtist(self):
+        return self.__artist
+    def getAlbum(self):
+        return self.__album
+class Picture(Media):
+    def __init__(self, media_type, name, rating, resolution):
+        self.__resolution = resolution
+        super().__init__(self)
     
+    def __str__(self):
+        return str(self.__media_type, self.__name, self.__rating, self.__resolution)
+    
+    def show(self):
+        print("Showing {}".format(self.__name))
+    
+    def getResolution(self):
+        return self.__resolution
+
